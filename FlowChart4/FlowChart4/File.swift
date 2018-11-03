33da753387770c50202680202ffc67ce12a644b8
Jessica Li <jessicali2019@berkeley.edu>
2018-11-03 12:23:43 -0700

    made a second pagegit status

---
+++ FlowChart4/FlowChart4/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  FlowChart4
+//
+//  Created by Jessica Li on 3/11/18.
+//  Copyright © 2018 Jessica Li. All rights reserved.
+//
+
+import UIKit
+
+class SecondPageViewController: UIViewController {
+    
+    
+    
+}
--- FlowChart4/FlowChart4/ViewController.swift
+++ FlowChart4/FlowChart4/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func tappedTest(_ sender: Any) {
+        self.performSegue(withIdentifier: "SecondPageSegue", sender: self)
+        
+    }
 
 }
 
